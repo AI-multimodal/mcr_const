27311a6684f3f445dc30ca199e5a97fa5c593ebf
Xiaohui Qu <xiaqu@bnl.gov>
2020-08-30 23:35:00 -0400

    add synthetic spectra generation code

---
+++ mcr/misc/generate_synthetic_spectra.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
+from typing import List, Tuple
+
+
+def generate_synthetic_spectra(step_pos: int, sigma: float, peaks: List[Tuple[int, int, int]], num_points: float = 250):
+    step_spec = np.ones(num_points)
+    step_spec[:step_pos] = 0
+
+    peak_spec = np.zeros(num_points)
+    for pos, inten, width in peaks:
+        peak_spec[pos - width//2:pos - width//2 + width] = inten
+
+    sharp_spec = step_spec + peak_spec
+    syn_spec = gaussian_filter1d(sharp_spec, sigma, mode='nearest')
+    return syn_spec
