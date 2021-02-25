a192a5d24f4b02d9a3569d09f71a5207848addb0
Xiaohui Qu <xiaqu@bnl.gov>
2021-02-25 16:02:15 -0500

    fix parameter name

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("requirements.txt") as f_req:
     required_list = [line.rstrip() for line in f_req.readlines()]
@@ -6,15 +6,12 @@ with open("requirements.txt") as f_req:
 setup(
     name='MCR_Constraints',
     version='0.1',
-    package=['mcr_const'],
+    packages=find_packages(),
     url='https://www.bnl.gov/cfn/',
     license='GPL',
     author='Xiaohui Qu',
     author_email='xiaqu@bnl.gov',
     description='Constraints to Embed Prior Knowledge in MCr',
     python_requires='>=3.7',
-    install_requires=required_list,
-    entry_points={
-        "console_scripts": []
-    }
+    install_requires=required_list
 )
