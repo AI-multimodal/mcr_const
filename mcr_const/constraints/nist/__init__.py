cd32b2b8617ccd73c515caa47af3f33c5c1e44a5
Xiaohui Qu <xhqu1981@gmail.com>
2022-02-16 14:38:11 -0500

    add fixed segment to global namespace

--- mcr_const/constraints/nist/__init__.py
+++ mcr_const/constraints/nist/__init__.py
@@ -1,4 +1,4 @@
-from .rank_selectivity import ConstraintPointBelow
+from .rank_selectivity import ConstraintPointBelow, ConstraintFixedSegment
 from .smooth import ConstraintSmooth, ConstraintConstant
 from .special_shape import halfgau_mod_gaussian, ConstraintGuinier, ConstraintWithFunction
 from .trilinear import SharedGlobalPrefactor, ConstraintGlobalPrefactor
